2000c15d14186ee6aef79e6ff9dbe4b0aa11a523
Vinoth Raja <vinoth.raja@monash.edu>
2023-08-06 20:03:23 +1000

    1.3 step3 Modify the variable - Part - definition

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,10 +1,16 @@
 const course = 'Half Stack application development';
-const part1 = 'Fundamentals of React';
-const exercises1 = 10;
-const part2 = 'Using props to pass data';
-const exercises2 = 7;
-const part3 = 'State of a component';
-const exercises3 = 14;
+const part1 = {
+  name: 'Fundamentals of React',
+  exercises: 10
+}
+const part2 = {
+  name: 'Using props to pass data',
+  exercises: 7
+}
+const part3 = {
+  name: 'State of a component',
+  exercises: 14
+}
 const Header = (props) => {
   return (
     <h1>{props.name}</h1>
@@ -13,9 +19,9 @@ const Header = (props) => {
 const Content = () => {
   return (
     <div>
-      <Part name ={part1} exercises = {exercises1} />
-      <Part name ={part2} exercises = {exercises2} />
-      <Part name ={part3} exercises = {exercises3} />
+      <Part name ={part1.name} exercises = {part1.exercises} />
+      <Part name ={part2.name} exercises = {part2.exercises} />
+      <Part name ={part3.name} exercises = {part3.exercises} />
     </div>
   );
 }
@@ -26,7 +32,7 @@ const Part = (props) => {
 }
 const Total = () => {
   return (
-    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
   );
 }
 const App = () => {
