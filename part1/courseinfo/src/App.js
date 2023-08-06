e9c9387e877aa4f898dac3c9a4ad12007e0ec108
Vinoth Raja <vinoth.raja@monash.edu>
2023-08-06 15:00:19 +1000

    1.2 Split Content into Parts

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -12,11 +12,16 @@ const Header = (props) => {
 }
 const Content = () => {
   return (
-    <>
-      <p> {part1} {exercises1} </p>
-      <p> {part2} {exercises2} </p>
-      <p> {part3} {exercises3} </p>
-    </>
+    <div>
+      <Part name ={part1} exercises = {exercises1} />
+      <Part name ={part2} exercises = {exercises2} />
+      <Part name ={part3} exercises = {exercises3} />
+    </div>
+  );
+}
+const Part = (props) => {
+  return (
+    <p> {props.name} {props.exercises} </p>
   );
 }
 const Total = () => {
