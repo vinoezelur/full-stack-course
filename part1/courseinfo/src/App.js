c7ef58f0960e5babdd8e8562d7615d9679f6fd17
Vinoth Raja <vinoth.raja@monash.edu>
2023-08-06 14:55:04 +1000

    1.1 Split App into 3 components

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,35 @@
+const course = 'Half Stack application development';
+const part1 = 'Fundamentals of React';
+const exercises1 = 10;
+const part2 = 'Using props to pass data';
+const exercises2 = 7;
+const part3 = 'State of a component';
+const exercises3 = 14;
+const Header = (props) => {
+  return (
+    <h1>{props.name}</h1>
+  );
+}
+const Content = () => {
+  return (
+    <>
+      <p> {part1} {exercises1} </p>
+      <p> {part2} {exercises2} </p>
+      <p> {part3} {exercises3} </p>
+    </>
+  );
+}
+const Total = () => {
+  return (
+    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+  );
+}
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header name = {course} />
+      <Content />
+      <Total />
     </div>
   )
 }
